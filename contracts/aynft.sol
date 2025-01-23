03e9c0d48451c0c9ed1b95a85fb39aa7937de9ae
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-01-23 19:09:33 +0100

    guess i won't be able to use URIStorage

--- contracts/aynft.sol
+++ contracts/aynft.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "@openzeppelin/con"
 contract AYNFT is ERC721 {
    uint256 tokenId = 0;
    constructor() ERC721("AYNFT","ANT"){}
    function mint(address _to) public{
+      require(_to != address(0), "Address needs to be valid");
       _safeMint(_to, tokenId);
       tokenId = tokenId + 1;
    }
